131a41d00343d9e691892028a1b41db06d9a583e
Gai | DV7 <167229955+xyzkyle@users.noreply.github.com>
2024-05-24 22:35:17 +0500

    Create main.lua

---
+++ --[[ Handler ]]--/main.lua
@@ -0,0 +1,11 @@
+------------------------ MAIN
+
+
+function c(--[[MAIN]])
+  print("Errors", ..errors..)
+end
+local function e()
+  os.exit()
+end
+if errors == nil then game:getButton("Resume Game")function() onclick("Resume Game") end end
+if errors == exist then print("Getting errors") print(errors) e() end
