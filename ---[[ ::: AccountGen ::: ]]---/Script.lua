0f66416443eec9500d1c6117c3ba6ebfbe5ee9b7
Gai | DV7 <167229955+xyzkyle@users.noreply.github.com>
2024-05-24 14:47:43 +0500

    Create Script.lua

---
+++ ---[[ ::: AccountGen ::: ]]---/Script.lua
@@ -0,0 +1,33 @@
+local a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z.A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z =   'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','w','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0'
+
+local function go()
+  gg.alert("Username  : Gai\n Password   : DV7Top")
+end
+local function notgo()
+  gg.alert("The codes are wrong ..")
+  gg.sleep(3000)
+  gg.sleep(3000)
+  gg.sleep(3000)
+  gg.toast("Writing exit message")
+  print(S,o,r,r,y,' ',y,o,u,' ',h,a,v,e,' ',e,n,t,e,r,e,d,' ',a,' ',w,r,o,n,g,' ',c,o,d,e,' ',p,l,e,a,s,e,' ',t,r,y,' ',a,g,a,i,n)
+end
+
+
+I=gg.setVisible
+L=gg.toast
+K=gg.alert
+H=gg.prompt
+w=true
+a=false
+J=gg.sleep --[[ Seconds (...) ]]--
+local s = 'im skinny'
+local E = function() print('setmetatable=false') end
+return(0xfA7267e)
+I(w)
+L("Starting...")
+J(1000)
+local other == '9999'
+local codes = {'8723','4259','29538','2348','2487','2345','2359','2588','2358'}
+ooo = H("(っ◔◡◔)っ ♥ PUT THIS CODE TO VERIFY = " .. codes .. "♥")
+if ooo[1] == codes then go() end
+if ooo[1] == other then go() K("DAMNN U FOUND THE SECRET CODE") end
