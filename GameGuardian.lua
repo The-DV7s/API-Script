9baf6e1400aa5f8c3884a1986a28a5fee2135e97
Gai | DV7 <167229955+xyzkyle@users.noreply.github.com>
2024-05-24 18:08:41 +0500

    Create GameGuardian.lua
    0------0

---
+++ GameGuardian.lua
@@ -0,0 +1,20 @@
+q=gg.toast
+w=gg.sleep
+e=gg.alert
+r=gg.cumonit
+-------
+--init
+------
+local revert = [[
+local appRequests = require("AppRequests")
+apprq.GetVersion(2.81.3)
+]]
+if gg == crash then io.output("Crashed") end
+--I=function(table.concat())
+if cmnt == true then else return nil end
+q("Success")
+if gg == not crash then else return nil end 
+if end_ == true then os.exit() q("Toast : ENded") end
+-- We do not obfuscate scripts, we only obfuscate important D:, so yeah ,no skidding pls
+require("api.codefixer")
+--Code fixer is needed to fix smol errors
