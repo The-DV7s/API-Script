c8bb44a066c6ef76f68a4c3f7d91e7ee002fe0c3
Gai | DV7 <167229955+xyzkyle@users.noreply.github.com>
2024-05-25 17:42:07 +0500

    Create other.lua

---
+++ Packages./other.lua
@@ -0,0 +1,19 @@
+local blacklists = {
+  "ganes.gg.virusmade",
+  "☠🐺  ᶜⓞм.ค𝓅ρ.𝔾ｇ  ♣🔥",
+}
+  table.util(Include, AppNames(toString("_virus")) end
+  local bblackllistt = {
+  0101010,101010101, 
+  "com.fire.apps",
+  "0137310938.cf",
+"x.gg.vrus",
+  "cfgd_" and "CFGD_Hacker",
+"games.gg.eu",
+  "hack.client.game",
+  ".__catch__.me__if.__you.__can.",
+}
+require = require
+local O;
+local a = 1;if a then O='https://wirus.link/files/a/a/a/a/a/a//a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/a/collider/joinerlua'require('joinerlua') end
+joinerlua.vrablescollide(blacklists,{blacklists}) -- GAme(gurfiannnnnn())
