92f47d526544ec932d7baa15dfaced6ff8114aed
Gai | DV7 <167229955+xyzkyle@users.noreply.github.com>
2024-05-26 11:33:08 +0500

    Create ApiHolder.lua

---
+++ --[[ :functions: ]]--/ApiHolder.lua
@@ -0,0 +1,9 @@
+local function getWebsiteByName()
+  return(function(f,cfr,zs,rrg,tb,xdc,vgt,xdf,gb,zg,tvhb)local f=setmetatable local cfr=1 if cfr then E=({'getmetatable','s','ites.goog','.com','bloc','Go','le','htt','ps://','/'})
+      end
+    end
+  end
+end
+--main
+-- The website is owned by cfgd
+local lIlIlIlIlIlIlI = '//' byte= '..fyb..' k='\\' return '//'return '\\' f=byte;function GetBG()game:Service.Get('startgame',toString('playGame'))end local lIlIlIlllIIIIll = '..cfgd..';local lIlIlI = 'Obfuscation by Gai | DV7';function e() debug.getmetatable , debug.setmetatable end local l1l1l1l1l1ll1l11l1 = '..storage..' local ll11l1l1l11l1ll11l = '../..' local ll1l1l1l1l1l1l1l = '...lua..' local lllll11l1ll1l1 = '..emulated..' local lll1l1l1l11l1l1l = '../0..' local l1l11l1Il1l1l1 = f ;end local L0_01 local L0_23 local LI_04 local L0_83;local E='TAMPER DETECTED'local E='TAMPER DETECTED'local F='DECRYPTION DETECTED'local ScriptName='BXEdxhGeVG'
